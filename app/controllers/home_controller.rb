94f7782e0b21aecdbf271b78786f329a59f211b6
Nick Gottlieb <ngottlieb@gmail.com>
2012-06-18 17:47:25 -0400

    pulling random video with simple api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @videos = Vimeo::Simple::Channel.videos("whitewater")
+    @video_to_play = @videos[rand(@videos.count) - 1]
   end
 end
