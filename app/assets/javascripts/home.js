232035c6f2324bdbbde0aae202164363c8e8e356
Nick Gottlieb <ngottlieb@gmail.com>
2012-06-18 20:15:31 -0400

    dryed up code

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
+	setNameVideoId();
 	$("iframe").hide();
 	var url_base = "http://player.vimeo.com/video/"
 	$("a#play-button").click(function() {
-		$.ajax({
-		  url: 'video',
-		  success: function(data) {
-		    $('a#play-button').attr("name", data);
-		  }
-		});
+		setNameVideoId();
 		
 		$("iframe").attr("src", (url_base + $("a#play-button").attr("name")));
 		$("iframe").show();
 	});
 });
+
+function setNameVideoId() {
+	$.ajax({
+	  url: 'video',
+	  success: function(data) {
+	    $('a#play-button').attr("name", data);
+	  }
+	});
+}
 
